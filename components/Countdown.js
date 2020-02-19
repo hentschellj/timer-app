238310675a036babd0618c6822fa30ce1516267d
hentschellj <hentschellj@gmail.com>
2020-02-18 18:58:36 -0600

    Set up skeleton for Countdown component

--- components/Countdown.js
+++ components/Countdown.js
@@ -9,6 +9,22 @@ class Countdown extends React.Component {
     return(
       <div>
         <h2>Time Remaining</h2>
+        <div className="digit">
+          <label htmlFor="hours">Hours</label><br />
+          <input type="number" name="hours" readOnly />
+        </div>
+
+        <div className="digit">
+          <label htmlFor="minutes">Minutes</label><br />
+          <input type="number" name="minutes" readOnly />
+        </div>
+
+        <div className="digit">
+          <label htmlFor="seconds">Seconds</label><br />
+          <input type="number" name="seconds" readOnly />
+        </div>
+
+        <button>Clear Timer</button>
       </div>
     )
   }
