f6e9616a968588d6cbde2a39467fd1ace15d0e80
hentschellj <hentschellj@gmail.com>
2020-02-18 18:41:21 -0600

    Created a component for time counting down

---
+++ components/Countdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Countdown extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return(
+      <div>
+        <h2>Time Remaining</h2>
+      </div>
+    )
+  }
+}
